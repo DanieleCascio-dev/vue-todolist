a5166e512bbcb58b9e888cd4b77d744b2c497c94
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2023-11-15 15:50:49 +0100

    add: list with vue

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ const { createApp } = Vue;
 
 const app = createApp({
   data() {
-    return {};
+    return {
+      toDo: {
+        whatToDo: "",
+      },
+      lista: [],
+    };
+  },
+  methods: {
+    addToList: function () {
+      console.log("Lista");
+      this.lista.push(this.toDo.whatToDo);
+      this.toDo.whatToDo = "";
+      console.log(this.lista);
+    },
   },
 }).mount("#app");
